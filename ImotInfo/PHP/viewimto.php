2baf150f711e833fabe650d487387c812861c31f
George-god <wesnoth7@gmail.com>
2022-04-01 22:16:50 +0300

    yes

--- ImotInfo/PHP/viewimto.php
+++ ImotInfo/PHP/viewimto.php
@@ -13,7 +13,7 @@ $imoterID = $_SESSION['userIm'];
 <head>
     <meta charset="UTF-8">
     <title>AdImot</title>
-    <link rel="stylesheet" type="text/css" href="../CSS/imotformeditS.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/imotformeditSview.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
@@ -25,8 +25,7 @@ $imoterID = $_SESSION['userIm'];
 </head>
 <body>
     <img src="../Pictures/LogoMaybe.png" alt="Italian Trulli" class="center">
-    <div>
-        <h1>View of Imot -> <?php echo $selection ?> </h1>
+    <div class="conui">
         <form action="index.php">
             <div class="container">
                 <?php
@@ -34,67 +33,67 @@ $imoterID = $_SESSION['userIm'];
                 $resultse = $conn->query($sqlse);
 
                 while($row = mysqli_fetch_array($resultse,MYSQLI_BOTH)) {
-                    $status = $row['status'];
                     $imid=$row['id'];
                 ?>
+                <div class="left">
                 <label for="Imotname">Name for Imot: <?php echo $row ['name']; ?></label><br>
-                <label for="cena">Price for Imot: <?php echo $row ['saleprice']; ?>.00 $</label><br>
-                <label for="renta">Rent for Imot: <?php echo $row ['rent']; ?>.00 $</label><br>
-                
-                <label for="status">Status:</label>
-                <select id="status" name="status" class="inptextmod1">
-                    <option value="rented" <?php if($status=="rented") echo 'selected="selected"'; ?> >rented</option>
-                    <option value="not_rented" <?php if($status=="not_rented") echo 'selected="selected"'; ?> >not_rented</option>
-                    <option value="for_sale" <?php if($status=="for_sale") echo 'selected="selected"'; ?> >for_sale</option>
-                </select>
-                <br>
+                <label for="cena">Price for Imot: <?php echo $row ['saleprice']; ?>.00 €</label><br>
+                <label for="renta">Rent for Imot: <?php echo $row ['rent']; ?>.00 €</label><br>
+                <label for="status">Status: <?php echo $row ['status']; ?></label><br>
+                </div>
                 <?php 
-                $sql = "SELECT * FROM imot_harakter WHERE imot_id='$imid'";
+                $sql = "SELECT * FROM imot_harakter INNER JOIN imottype on imot_harakter.imottype = imottype.typeid WHERE imot_id='$imid'";
                 $result = $conn->query($sql);
 
                 while($rower = mysqli_fetch_array($result,MYSQLI_BOTH)) {
                     $water = $rower['water'];
                     $gas = $rower['gas'];
                     $tok = $rower['electricity'];
-                    $type = $rower['imottype'];
                 ?>
+                    <div class="right">
+                        <div class="rightleft">
+                        <label for="type">What type: <?php echo $rower['type']; ?></label><br>
 
-                    <label for="type">What type:</label>
-                    <select id="type" name="type" class="inptextmod1">
-                        <?php
-                            $sqlt = mysqli_query($conn, "SELECT * From imottype");
-                                $rowt = mysqli_num_rows($sqlt);
-                                while ($rowt = mysqli_fetch_array($sqlt)){
-                                echo "<option value='". $rowt['typeid'] ."' ".(($type==$rowt['typeid'])?'selected="selected"':"")." >" .$rowt['type'] ."</option>" ;
-                            }
-                        ?>
-                    </select>
+                        <label for="water">Water:
+                            <?php
+                                if($water=="1"){
+                                    echo "YES";
+                                }
+                                else {
+                                    echo "NO";
+                                }
+                            ?>
+                        </label><br>
 
-                    <label for="water">Water:</label>
-                    <select id="water" name="water" class="inptextmod1">
-                        <option value="1" <?php if($water=="1") echo 'selected="selected"'; ?> >YES</option>
-                        <option value="0" <?php if($water=="0") echo 'selected="selected"'; ?> >NO</option>
-                    </select>
+                        <label for="gas">Gas:
+                            <?php
+                                if($gas=="1"){
+                                    echo "YES";
+                                }
+                                else {
+                                    echo "NO";
+                                }
+                            ?>
+                        </label><br>
 
-                    <label for="cenavoda">Cena na voda: <?php echo $rower['waterprice']; ?>.00 $</label><br>
-
-                    <label for="gas">Gas:</label>
-                    <select id="gas" name="gas" class="inptextmod1">
-                        <option value="1" <?php if($gas=="1") echo 'selected="selected"'; ?> >YES</option>
-                        <option value="0" <?php if($gas=="0") echo 'selected="selected"'; ?> >NO</option>
-                    </select>
-
-                    <label for="cenagas">Cena na gasta: <?php echo $rower['gasprice']; ?>.00 $</label><br>
-
-                    <label for="tok">Tok:</label>
-                    <select id="tok" name="tok" class="inptextmod1">
-                        <option value="1" <?php if($tok=="1") echo 'selected="selected"'; ?> >YES</option>
-                        <option value="0" <?php if($tok=="0") echo 'selected="selected"'; ?> >NO</option>
-                    </select>
-
-                    <label for="cenatok">Cena na toka: <?php echo $rower['electricityprice']; ?>.00 $</label><br>
-
-                    <label for="kvadratura">Kvadratura: <?php echo $rower['kvadrat']; ?> kv.m</label> <br>                 
+                        <label for="tok">Tok:
+                            <?php
+                                if($tok=="1"){
+                                    echo "YES";
+                                }
+                                else {
+                                    echo "NO";
+                                }
+                            ?>
+                        </label> <br>              
+                        </div>
+                        <div class="rightright">
+                            <label for="cenatok">Cena na toka: <?php echo $rower['electricityprice']; ?>.00 €</label><br>
+                            <label for="cenavoda">Cena na voda: <?php echo $rower['waterprice']; ?>.00 €</label><br>
+                            <label for="cenagas">Cena na gasta: <?php echo $rower['gasprice']; ?>.00 €</label><br>
+                            <label for="kvadratura">Kvadratura: <?php echo $rower['kvadrat']; ?> ㎡</label> <br> 
+                        </div>
+                    </div>                
 
                 <input type="submit" value="Done Viewing" class="inpsubmod1">                
             </div>
